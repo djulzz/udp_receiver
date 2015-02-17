b5fa3e069e7e91cd63da2c88220530b5c9b60fa0
djulzz1982 <julien.esposito@gmail.com>
2015-02-16 16:42:30 -0800

    Added a Test with Pseudo Kalman Filtering
    Added a Test with Pseudo Kalman Filtering

--- debug/main_analyze_data.m
+++ debug/main_analyze_data.m
@@ -3,7 +3,8 @@ clear all;
 close all;
 clc;
 
-options.doPlot_accelerometer = true;
+options.doPlot_accelerometer = false;
+options.doSave_data = true;
 
 format = '%c, %li, %i, %f, %f, %f\n';
 
@@ -97,3 +98,135 @@ avg_Fs_g = 1 / avg_T_g;
 fprintf( 'Fs_a = %.2f Hz\n', avg_Fs_a );
 fprintf( 'Fs_m = %.2f Hz\n', avg_Fs_m );
 fprintf( 'Fs_g = %.2f Hz\n', avg_Fs_g );
+
+upper_t = max( [array_accels( end, 1 ), array_mag( end, 1 ), array_gyros( end, 1 )] );
+lower_t = min( [array_accels( 1, 1 ), array_mag( 1, 1 ), array_gyros( 1, 1 )] );
+timespan_ms = [lower_t, upper_t];
+
+nElements_a = size( array_accels( :, 1 ), 1 );
+nElements_m = size( array_mag( :, 1 ), 1 );
+nElements_o = size( array_gyros( :, 1 ), 1 );
+
+same_size_tf = ( ( nElements_a == nElements_m ) && ( nElements_m == nElements_o ) );
+if( true == same_size_tf )
+    directory = 'C:\Users\djulzz\Desktop\madgwick_algorithm_matlab\';
+    filename = '02_16_2015.mat';
+    A_SI_to_Gs = ( 1 / 9.81 );
+    G_SI_to_DegS = ( 180 / pi );
+    uT_to_Gauss = 0.01;
+    ms_to_Sec = ( 1 / 1000 );
+    % 1 Tesla = 10 000 Gauss
+    Accelerometer   = A_SI_to_Gs * array_accels( :, 2 : 1 : 4 ); % m / s / s
+    Gyroscope       = G_SI_to_DegS * array_gyros( :, 2 : 1 : 4 ); % rad / s
+    Magnetometer    = uT_to_Gauss * array_mag( :, 2 : 1 : 4 ); % Micro Tesla
+    time            = ms_to_Sec * linspace( lower_t, upper_t, nElements_o);
+    if( true == options.doSave_data )
+        save( [directory ,filename], 'Accelerometer', 'Gyroscope', 'Magnetometer','time' );
+        fprintf( 'Saved!\n' );
+    end
+
+    n = nElements_o;
+    z = zeros(n,1);
+    % corrected state vectors
+    x = zeros(n,1);
+    y = zeros(n,1);
+    z = zeros(n,1);
+    % corrected error co-variance matrix
+    P_x = ones(n,1);
+    P_y = ones(n,1);
+    P_z = ones(n,1);
+    % predicted state vectors
+    x_p = zeros(n,1);
+    x_p = zeros(n,1);
+    x_p = zeros(n,1);
+    % predicted error co-variance matrix
+    P_p_x = zeros(n,1);
+    P_p_y = zeros(n,1);
+    P_p_z = zeros(n,1);
+    %P_p(1) = 2;
+
+    % variance of disturbance noise
+    Q = .01; 
+    % variance of sensor noise
+    R = .1;
+
+    for k=1:n-1
+
+        sensor_x(k+1) = Accelerometer( k, 1 );
+        sensor_y(k+1) = Accelerometer( k, 2 );
+        sensor_z(k+1) = Accelerometer( k, 3 );
+
+
+        % Z axis
+        % prediction
+
+        z_p(k+1) = z(k);
+        P_p_z(k+1) = P_z(k) + Q;
+
+        % correction
+
+        K_z = P_p_z(k+1)/(P_p_z(k+1) + R);
+        z(k+1) = z_p(k+1) + K_z*(sensor_z(k+1) - z_p(k+1));
+        P_z(k+1) = (1 - K_z)* P_p_z(k+1);
+
+        % plotting
+
+        title('Z axis accelerometer ');
+        subplot(3,1,1);
+        plot(k,z(k),'-ro',k,sensor_z(k), '-b*');
+        axis([0 k+10 0 20]);
+        pause(.0001);
+        drawnow
+        grid on
+        hold on;
+
+        % Y axis
+        % prediction
+
+        y_p(k+1) = y(k);
+        P_p_y(k+1) = P_y(k) + Q;
+
+        % correction
+
+        K_y = P_p_y(k+1)/(P_p_y(k+1) + R);
+        y(k+1) = y_p(k+1) + K_y*(sensor_y(k+1) - y_p(k+1));
+        P_y(k+1) = (1 - K_y)* P_p_y(k+1);
+
+        % plotting
+
+        title('Y axis accelerometer ');
+        subplot(3,1,2);
+        plot(k,y(k),'-ro',k,sensor_y(k), '-b*');
+        axis([0 k+10 0 20]);
+        pause(.0001);
+        drawnow
+        grid on
+        hold on;
+
+        % X axis
+        % prediction
+
+        x_p(k+1) = x(k);
+        P_p_x(k+1) = P_x(k) + Q;
+
+        % correction
+
+        K_x = P_p_x(k+1)/(P_p_x(k+1) + R);
+        x(k+1) = x_p(k+1) + K_x*(sensor_x(k+1) - x_p(k+1));
+        P_x(k+1) = (1 - K_x)* P_p_x(k+1);
+
+        % plotting
+
+        title('X axis accelerometer ');
+        subplot(3,1,3);
+        plot(k,x(k),'-ro',k,sensor_x(k), '-b*');
+        axis([0 k+10 0 20]);
+        pause(.0001);
+        drawnow
+        grid on
+        hold on;
+
+    end
+    
+    
+end
